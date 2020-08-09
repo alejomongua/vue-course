8bbf1a365635ec635ef5fd003db8f12914f05b1c
Alejandro Mongua <alejandromongua@gmail.com>
2020-08-08 23:38:04 -0500

    Commit inicial, primeros pasos en vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class='container'>
+      <Skills />
+    </div>
+  </div>
+</template>
+
+<script>
+import Skills from './components/Skills.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Skills
+  }
+}
+</script>
+
+<style>
+@import url('https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css')
+
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>Skills</h1>
+    <ul>
+      <li v-for='(data, index) in skills' :key='index'>
+        {{ index + 1 }} -  {{ data }}
+      </li>
+    </ul>
+
+
+    <div v-if='!!alert' class='alert'>{{alert}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data () {
+    return {
+      skills: [
+        'react',
+        'vue',
+        'ssr',
+        'python',
+        'javascript',
+        'ruby'
+      ],
+      alert: null,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+.alert {
+  background-color: lightpink;
+  color: white;
+}
+</style>
